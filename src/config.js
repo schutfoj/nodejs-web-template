15ccb675f8e81b163d412190480a08dae91a2056
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-11-26 08:52:56 -0800

    feat: add basic template functionality

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// TODO: Add your database configuration to this file.
+// NOTE: If you are using source code control (like GitHub), make sure you 
+//       do not check in passwords!
+//
+module.exports = {
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_<username>',
+  password: 'XXXX',
+  database: 'cs340_<username>'
+};
